fce0d666629b912d774f909f540a940c3a37dcc2
mask <617473294@qq.com>
2025-03-24 16:56:48 +0800

    c

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -28,7 +28,8 @@ const disableDebug: boolean = process.env.OPENAI_API_DISABLE_DEBUG === 'true'
 
 let apiModel: ApiModel
 const model = isNotEmptyString(process.env.OPENAI_API_MODEL) ? process.env.OPENAI_API_MODEL : 'gpt-3.5-turbo'
-
+console.log(isNotEmptyString(process.env.OPENAI_API_KEY), 'key')
+console.log(isNotEmptyString(process.env.OPENAI_ACCESS_TOKEN))
 if (!isNotEmptyString(process.env.OPENAI_API_KEY) && !isNotEmptyString(process.env.OPENAI_ACCESS_TOKEN))
   throw new Error('Missing OPENAI_API_KEY or OPENAI_ACCESS_TOKEN environment variable')
 
